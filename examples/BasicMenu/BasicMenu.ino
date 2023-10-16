1bd4b528900ef921f0b411d944b6cfff2cec851f
hori4 <ch.horisberger@gmail.com>
2023-10-16 22:36:45 +0200

    added example

---
+++ examples/BasicMenu/BasicMenu.ino
@@ -0,0 +1,27 @@
+#include <M5Stack.h>
+#include <M5StackMenuSystem.h>
+
+Menu myMenu("Main Menu");
+
+void setup() {
+  M5.begin();
+  M5.Power.begin();
+ 
+  myMenu.addItem(new CallbackMenuItem("Menu Item #1", testOneTimeCallback));
+  myMenu.addItem(new CallbackMenuItem("Menu Item #2", testOneTimeCallback));
+  myMenu.addItem(new CallbackMenuItem("Menu Item #3", testOneTimeCallback));
+}
+
+void loop() {
+  M5.update();
+  myMenu.loop();
+}
+
+void testOneTimeCallback(CallbackMenuItem& menuItem)
+{
+  M5.Lcd.clear(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextDatum(MC_DATUM);
+  M5.Lcd.drawString(menuItem.getText() + " selected", 160, 120);
+  delay(1000);
+}
